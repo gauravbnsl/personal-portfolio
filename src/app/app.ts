1d114513720725663c4c7a5dc805f2e09b009b9c
Gaurav Bansal <gauravbansal67@hotmail.com>
2025-08-16 15:30:29 +0530

    Added profile sections

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { About } from './about/about';
+import { Skills } from './skills/skills';
+import { Experience } from './experience/experience';
+import { Education } from './education/education';
+import { Projects } from './projects/projects';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, About, Skills, Experience, Education, Projects, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
